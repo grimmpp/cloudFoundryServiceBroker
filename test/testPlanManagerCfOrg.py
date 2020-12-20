02a54cfc9cffdfbc52995bd2a7bb4ca47953f08f
Philipp Grimm <grimm.philipp@gmail.com>
2020-12-20 15:05:22 +0100

    test added

--- test/testPlanManagerCfOrg.py
+++ test/testPlanManagerCfOrg.py
@@ -5,6 +5,7 @@ from openbrokerapi.catalog import ServicePlan
 from cfBroker.planManagerCfOrg import PlanManagerCfOrg
 from cfBroker.cfClient import CfClient
 from cfBroker.applicationSettings import ApplicationSettings
+from openbrokerapi.service_broker import ProvisionDetails
 
 class TestPlanManagerCfOrg(unittest.TestCase):
 
@@ -18,6 +19,10 @@ class TestPlanManagerCfOrg(unittest.TestCase):
         self.assertEqual('CF Org', servicePlan.name)
         self.assertFalse(servicePlan.bindable)
 
+    def test_prov(self):
+        instanceId = '29dcc9ea-629c-4307-b0fb-a0f6c047e788'
+        details = ProvisionDetails('service_id', 'plan_id', 'organization_guid', 'space_guid', parameters = {'name': 'test org'})
+        self.planManager.provision(instanceId, details, async_allowed=True)
 
 if __name__ == '__main__':
     unittest.main()
